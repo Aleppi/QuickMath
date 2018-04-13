cd0155512737e9cc154fc35ec3d816eb21fac756
Aleppi <hectortib@gmail.com>
2018-04-13 12:53:14 +0200

    Added dot and cross product member functions

--- LinAlg/vector.cpp
+++ LinAlg/vector.cpp
@@ -30,3 +30,18 @@ Vector& Vector::scalarMultiply(int scalar)
     m_z *= scalar;
     return *this;
 }
+
+int Vector::dotProduct(std::array<int, 3> vector)
+{
+    int result = m_x * vector[1] + m_y * vector[2] + m_z * vector[3];
+    return result;
+}
+
+Vector Vector::crossProduct(std::array<int, 3> vector)
+{
+    int x = m_y * vector[3] - m_z * vector[2];
+    int y = m_z * vector[1] - m_x * vector[3];
+    int z = m_x * vector[2] - m_y * vector[1];
+    Vector result(x, y, z);
+    return result;
+}
--- LinAlg/vector.h
+++ LinAlg/vector.h
@@ -10,6 +10,8 @@ class Vector {
         Vector& addVector(std::array<int, 3> vector);
         Vector& subtractVector(std::array<int, 3> vector);
         Vector& scalarMultiply(int scalar);
+        int dotProduct(std::array<int, 3> vector);
+        Vector crossProduct(std::array<int, 3> vector);
     private:
         int m_x = 0;
         int m_y = 0;
