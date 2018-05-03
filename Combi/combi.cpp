dba223a554dff9437bd8071ac9dc4b6859194956
Aleppi <hectortib@gmail.com>
2018-05-03 12:07:10 +0200

    Added base combi class

---
+++ Combi/combi.cpp
@@ -0,0 +1,13 @@
+#include "combi.h"
+
+int calculatePermutations(int n, int k)
+{
+    int permutations;
+    return permutations;
+}
+
+int calculateCombinations(int n, int k)
+{
+    int combinations;
+    return combinations;
+}
---
+++ Combi/combi.h
@@ -0,0 +1,9 @@
+#ifndef COMBI_H
+#define COMBI_H
+class Combi {
+    public:
+        Combi() {};
+        int calculatePermutations(int n, int k);
+        int calculateCombinations(int n, int k);
+}
+#endif
